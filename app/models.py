67222983705a9886eae937cfcddb2ecd536f8099
nimishp7 <nimishp7@gmail.com>
2023-03-14 09:19:45 +0530

    second commit

--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.core.validators import MaxLengthValidator,MinLengthValidator
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 
 # Create your models here.
 
 STATE_CHOICES = (
-    ('Andaman & Nicobar Islands', 'Andaman & Nicobar Islands' ),
+    ('Andaman & Nicobar Islands', 'Andaman & Nicobar Islands'),
     ('Andhra Pradesh', 'Andhra Pradesh'),
     ('Arunachal pradesh', 'Arunachal pradesh'),
     ('Assam', 'Assam'),
@@ -18,60 +18,66 @@ STATE_CHOICES = (
     ('Delhi', 'Delhi'),
     ('Madhya Pradesh', 'Madhya Pradesh'),
     ('Utter Pradesh', 'Utter Pradesh'),
-    ('Tamilnadu','Tamilnadu'),
+    ('Tamilnadu', 'Tamilnadu'),
 )
+
 
 class Customer(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    locality=models.CharField(max_length=200)
+    locality = models.CharField(max_length=200)
     city = models.CharField(max_length=50)
     zipcode = models.IntegerField()
-    state = models.CharField(choices=STATE_CHOICES,max_length=50)
-    
+    state = models.CharField(choices=STATE_CHOICES, max_length=50)
+
     def __str__(self):
         return str(self.id)
-    
-CATEGORY_CHOICES =(
+
+
+CATEGORY_CHOICES = (
     ('M', 'Models'),
-    ('L','Laptop'),
-    ('TW','Top Wear'),
+    ('L', 'Laptop'),
+    ('TW', 'Top Wear'),
     ('BW', 'Bottom Wear'),
-)    
+)
+
 
 class Product(models.Model):
     title = models.CharField(max_length=100)
-    selling_price=models.FloatField()
-    discounted_price=models.FloatField()
-    description=models.TextField()
+    selling_price = models.FloatField()
+    discounted_price = models.FloatField()
+    description = models.TextField()
     brand = models.CharField(max_length=200)
-    category = models.CharField(choices = CATEGORY_CHOICES,max_length=2)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=2)
     product_image = models.ImageField(upload_to='producting')
-    
+
     def __str__(self):
         return str(self.id)
-    
-    
+
+
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default =1)
-        
+    quantity = models.PositiveIntegerField(default=1)
+
     def __str__(self):
         return str(self.id)
-    
+
+
 STAUS_CHOICES = (
     ('Accepted', 'Accepted'),
-    ('packed','packed'),
-    ('On The Way','On The Way'),
+    ('packed', 'packed'),
+    ('On The Way', 'On The Way'),
     ('Delivered', 'Delivered'),
-    ('Cancel','Cancel')
-)    
+    ('Cancel', 'Cancel')
+)
+
 
 class OrderPlaced(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField(default =1)
+    quantity = models.PositiveIntegerField(default=1)
     ordered_date = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length =50, choices = STAUS_CHOICES,default='Pending')
+    status = models.CharField(
+        max_length=50, choices=STAUS_CHOICES, default='Pending')
--- app/views.py
+++ app/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Customer, Product, Cart, OrderPlaced
+from django.db.models import Q
 
 
 class ProductView(View):
     def get(self, request):
-        topwears = Product.objects.filter(category= 'TW')
-        bottomwears = Product.objects.filter(category= 'BW')
-        mobiles = Product.objects.filter(category= 'M')
-        laptops = Product.objects.filter(category= 'L')
-        return render(request, 'app/home.html',{'topwears':topwears, 'bottomwears':bottomwears, 'mobiles':mobiles, 'laptops':laptops})
+        topwears = Product.objects.filter(category='TW')
+        bottomwears = Product.objects.filter(category='BW')
+        mobiles = Product.objects.filter(category='M')
+        laptops = Product.objects.filter(category='L')
+        return render(request, 'app/home.html', {'topwears': topwears, 'bottomwears': bottomwears, 'mobiles': mobiles, 'laptops': laptops})
 
 
 # def product_detail(request):
@@ -19,45 +20,52 @@ class ProductDetailView(View):
         product = Product.objects.get(pk=pk)
         item_already_in_cart = False
         if request.user.is_authenticated:
-            item_already_in_cart = Cart.objects.filter(Q(product=product.id) & Q(user=request.user)).exists()
-            return render(request, 'app/productdetail.html',{'product':product, 'item_already_in_cart':item_already_in_cart})
+            item_already_in_cart = Cart.objects.filter(
+                Q(product=product.id) & Q(user=request.user)).exists()
+            return render(request, 'app/productdetail.html', {'product': product, 'item_already_in_cart': item_already_in_cart})
         else:
-            return render(request, 'app/productdetail.html',{'product':product, 'item_already_in_cart':item_already_in_cart})
+            return render(request, 'app/productdetail.html', {'product': product, 'item_already_in_cart': item_already_in_cart})
 
-
-        
-def home(request):
- return render(request, 'app/home.html')
 
 def product_detail(request):
- return render(request, 'app/productdetail.html')
+    return render(request, 'app/productdetail.html')
+
 
 def add_to_cart(request):
- return render(request, 'app/addtocart.html')
+    return render(request, 'app/addtocart.html')
+
 
 def buy_now(request):
- return render(request, 'app/buynow.html')
+    return render(request, 'app/buynow.html')
+
 
 def profile(request):
- return render(request, 'app/profile.html')
+    return render(request, 'app/profile.html')
+
 
 def address(request):
- return render(request, 'app/address.html')
+    return render(request, 'app/address.html')
+
 
 def orders(request):
- return render(request, 'app/orders.html')
+    return render(request, 'app/orders.html')
+
 
 def change_password(request):
- return render(request, 'app/changepassword.html')
+    return render(request, 'app/changepassword.html')
+
 
 def mobile(request):
- return render(request, 'app/mobile.html')
+    return render(request, 'app/mobile.html')
+
 
 def login(request):
- return render(request, 'app/login.html')
+    return render(request, 'app/login.html')
+
 
 def customerregistration(request):
- return render(request, 'app/customerregistration.html')
+    return render(request, 'app/customerregistration.html')
+
 
 def checkout(request):
- return render(request, 'app/checkout.html')
+    return render(request, 'app/checkout.html')
