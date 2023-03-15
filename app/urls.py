a87740df9bda310f8c4f2d72d91d91e7a6686d1b
nimishp7 <nimishp7@gmail.com>
2023-03-15 09:27:47 +0530

    3 commit

--- app/urls.py
+++ app/urls.py
@@ -5,8 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.ProductView.as_view(), name='home'),
-    path('product-detail/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
-    path('cart/', views.add_to_cart, name='add-to-cart'),
+    path('product-detail/<int:pk>',
+         views.ProductDetailView.as_view(), name='product-detail'),
+    path('add-to-cart/', views.add_to_cart, name='add-to-cart'),
+    path('cart/', views.show_cart, name='showcart'),
     path('buy/', views.buy_now, name='buy-now'),
     path('profile/', views.profile, name='profile'),
     path('address/', views.address, name='address'),
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Customer, Product, Cart, OrderPlaced
 from django.db.models import Q
@@ -27,12 +27,39 @@ class ProductDetailView(View):
             return render(request, 'app/productdetail.html', {'product': product, 'item_already_in_cart': item_already_in_cart})
 
 
-def product_detail(request):
-    return render(request, 'app/productdetail.html')
-
-
 def add_to_cart(request):
-    return render(request, 'app/addtocart.html')
+    user = request.user
+    product_id = request.GET.get('prod_id')
+    product = Product.objects.get(id=product_id)
+    Cart(user=user, product=product).save()
+    return redirect('/cart')
+
+
+def show_cart(request):
+    if request.user.is_authenticated:
+        user = request.user
+        cart = Cart.objects.filter(user=user)
+        amount = 0.0
+        shipping_amount = 70.0
+        total_amount = 0.0
+        cart_product = [p for p in Cart.objects.all() if p.user == user]
+        print(cart_product)
+        if cart_product:
+            for p in cart_product:
+                tempamount = (p.quantity*p.product.discounted_price)
+                amount += tempamount
+                total_amount = amount+shipping_amount
+            return render(request, 'app/addtocart.html', {'carts': cart, 'total_amount': total_amount, 'amount': amount})
+        else:
+            return render(request, 'app/emptycart.html')
+
+
+# def product_detail(request):
+#     return render(request, 'app/productdetail.html')
+
+
+# def add_to_cart(request):
+#     return render(request, 'app/addtocart.html')
 
 
 def buy_now(request):
